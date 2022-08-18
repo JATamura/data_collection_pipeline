35f6064e4f582aecdb1bd251c20a10f8a0a0a940
JATamura <jatamura1125@gmail.com>
2022-08-18 21:26:31 +0100

    complete scraper class

--- scraper.py
+++ scraper.py
@@ -7,17 +7,20 @@ import os
 import json
 import urllib.request
 
-import time
-import cv2
-import matplotlib.pyplot as plt
-from PIL import Image
-import io
-
 class scraper:
     def __init__(self, URL):
         self.driver = webdriver.Chrome()
-        link = self.find_link(URL)
-        self.click_link(link)
+        self.links = self.find_links(URL)
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        self.regions = soup.find_all(name = "div", attrs = {"class": "panel-box-heading"})
+        if not os.path.exists("raw_data"):
+            os.makedirs("raw_data")
+        if not os.path.exists("raw_data\images"):
+            os.makedirs("raw_data\images")
+        for r in self.regions:
+            if not os.path.exists("raw_data\images\\" + r.text):
+                os.makedirs("raw_data\images\\" + r.text)
 
     def click_link(self, URL):
         self.driver.get(URL)
@@ -25,14 +28,20 @@ class scraper:
     def scroll_to_bottom(self):
         self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
-    def find_link(self, URL):
+    def find_links(self, URL):
         self.click_link(URL)
         content = self.driver.find_element(by=By.XPATH, value='//*[@id="mw-content-text"]')
-        table = content.find_element(by=By.CLASS_NAME, value="panel-box-body")
-        team = table.find_element(by=By.CLASS_NAME, value="team-template-text")
-        a_tag = team.find_element(by=By.TAG_NAME, value='a')
-        link = a_tag.get_attribute('href')
-        return link
+        tables = content.find_elements(by=By.CLASS_NAME, value="panel-box-body")
+        links = []
+        for t in tables:
+            teams_in_region = []
+            teams = t.find_elements(by=By.CLASS_NAME, value="team-template-text")
+            for team in teams:
+                a_tag = team.find_element(by=By.TAG_NAME, value='a')
+                link = a_tag.get_attribute('href')
+                teams_in_region.append(link)
+            links.append(teams_in_region)
+        return links
 
     def collect_team_data(self, soup):
         side_box = soup.find(name = "div", attrs = {"class": "fo-nttax-infobox"})
@@ -40,7 +49,7 @@ class scraper:
         info_dict = {}
         for info in side_info[0::2]:
             info_dict[info.text.replace("\xa0", " ")] = side_info[side_info.index(info)+1].text.replace("\xa0", " ")
-        return(info_dict)
+        return info_dict
     
     def collect_player_data(self, soup):
         player_box = soup.find(name = "div", attrs = {"class": "table-responsive"})
@@ -50,7 +59,7 @@ class scraper:
             p_row = p.find_all("td")
             player_dict = {"ID": p_row[0].text.replace("\xa0", " "), "Name": p_row[2].text.replace("\xa0", " ")}
             info_dict[p_row[3].text.replace("\xa0", " ")] = player_dict
-        return(info_dict)
+        return info_dict
 
     def collect_image_data(self):
         content = self.driver.find_element(by=By.XPATH, value='//*[@id="mw-content-text"]')
@@ -60,11 +69,18 @@ class scraper:
         image = self.driver.find_element(by=By.CLASS_NAME, value="fullImageLink")
         a_tag = image.find_element(by=By.TAG_NAME, value="a")
         src = a_tag.get_attribute("href")
-        return(src)
+        return src
 
     def generate_id(self):
         content = self.driver.find_element(by=By.XPATH, value='//*[@id="firstHeading"]')
-        return(content.text, str(uuid.uuid4()))
+        return [content.text, str(uuid.uuid4())]
+    
+    def save_image_data(self, data, region):
+        urllib.request.urlretrieve(data["Logo Data"], "raw_data\images\\" + region  + "\\" + data["ID"][0] + ".png")
+
+    def save_text_data(self, all_data):
+        with open('raw_data\data.json', 'w') as f:
+            json.dump(all_data, f)
 
     def collect_data(self, URL):
         page = requests.get(URL)
@@ -74,20 +90,24 @@ class scraper:
         data["Team Data"] = self.collect_team_data(soup)
         data["Players Data"] = self.collect_player_data(soup)
         data["Logo Data"] = self.collect_image_data()
-        print(data)
-        return(data)
-    
-    def save_data(self, data):
-        if not os.path.exists("raw_data"):
-            os.makedirs("raw_data")
-        with open('raw_data\data.json', 'w') as d:
-            json.dump(data, d)
-        if not os.path.exists("raw_data\images"):
-            os.makedirs("raw_data\images")
-        urllib.request.urlretrieve(data["Logo Data"], "raw_data\images\\" + data["ID"][0] + ".png")
+        return data
+
+    def collect_all_data(self):
+        i = 0
+        all_data = {}
+        for links in self.links:
+            curr_region = self.regions[i].text
+            all_data[curr_region] = []
+            for link in links:
+                self.click_link(link)
+                data = s.collect_data(link)
+                s.save_image_data(data, curr_region)
+                all_data[curr_region].append(data)
+            i += 1
+        self.save_text_data(all_data)
 
 if __name__ == "__main__":
     s = scraper("https://liquipedia.net/leagueoflegends/Portal:Teams")
-    data = s.collect_data("https://liquipedia.net/leagueoflegends/100_Thieves")
-    s.save_data(data)
+    s.collect_all_data()
+    
     
